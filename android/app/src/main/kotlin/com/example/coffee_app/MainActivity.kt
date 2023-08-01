8def289a70767e8d6fa8f3e67d925608c8592351
Froloveee <vadim.frolo.chelny02@gmail.com>
2023-08-01 16:39:38 +0300

    First Yandex Maps Commit

--- android/app/src/main/kotlin/com/example/coffee_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/coffee_app/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.coffee_app
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("223e7ec5-e632-417a-b494-0cf6b865c0a3") // Your generated API key
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
