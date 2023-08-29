7f7fa9187d1584ab765f1ada735318952be8fb35
liltymoor <timur.chelter@mail.ru>
2023-08-29 23:10:45 +0300

    added standard port listening with express app

---
+++ index.js
@@ -0,0 +1,16 @@
+// IMPORTS 
+// =========================
+
+require('dotenv').config();
+
+const express = require("express");
+
+// APP INIT
+// =========================
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.listen(PORT, () => {console.log('Server started on port ' + PORT)});
+
+// =========================
