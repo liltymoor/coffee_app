834310c355ddadf549371de4ce16d23b31ee3614
liltymoor <timur.chelter@mail.ru>
2023-09-17 14:15:21 +0300

    added counter to logger

--- routes/logRoute.js
+++ routes/logRoute.js
@@ -1,5 +1,8 @@
 const { connectionHistory } = require('../models');
 
 module.exports = function (req, res, next) {
-    res.status(200).json(connectionHistory.findAll());
+    const serverHistory   = connectionHistory.findAll();
+    const connectionCount = connectionHistory.count();
+
+    res.status(200).json({count: connectionCount, history: serverHistory});
 }
